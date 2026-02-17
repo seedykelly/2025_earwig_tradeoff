0b3332419d5bbb4c37db7840292820e76c195037
clint kelly <kelly.clint@uqam.ca>
2026-02-17 09:52:01 -0500

    Downgraded R version to get brms working with stan and flextable.

--- scripts/earwig_tradeoff.R
+++ scripts/earwig_tradeoff.R
@@ -16,8 +16,14 @@ library(broman)
 library(tidyverse)
 library(smatr)
 library(factoextra)
+library(officer)
+library(flextable)
+library(tidyverse)
+library(rptR)
 
-#### Load data ####
+# =========================================
+# Load data
+# =========================================
 earwig_body<-read.csv("data/raw/earwig_body_measurements.csv")
 list_of_files <- list.files(path = "data/raw/egg_data_files",
                             recursive = TRUE,
@@ -27,7 +33,9 @@ earwig_egg <- readr::read_csv(list_of_files)
 earwig_egg$num <- as.factor(earwig_egg$num)
 earwig_egg$id <- as.factor(earwig_egg$id)
 
-#### Trait repeatability ####
+# =========================================
+# Trait repeatability
+# =========================================
 # body morphology
 # make wide body data long
 data_long.body <- gather(earwig_body, trait, measurement, pronotum_length_1:body_mass_3, factor_key=TRUE)
@@ -60,8 +68,9 @@ earwig_body_2 <- earwig_body %>%
   dplyr::select(-c(pronotum_length_1, pronotum_length_2, pronotum_length_3,body_mass_1, body_mass_2, body_mass_3)) %>%
   ungroup()
 
-#### Principal components ####
-
+# =========================================
+# Principal components
+# =========================================
 pc <- prcomp(earwig_body_2[,c(-1, -2)],
              center = TRUE,
              scale. = TRUE) # scale = TRUE causes PCA on correlation matrix
@@ -78,7 +87,9 @@ res.var$coord          # Coordinates
 res.var$contrib        # Contributions to the PCs
 res.var$cos2           # Quality of representation 
 
-#### Scaled mass index ####
+# =========================================
+# Scaled mass index
+# =========================================
 fit<-sma(log(earwig_body_2$mean.mass)~log(earwig_body_2$mean.pronotum))
 summary(fit)
 plot(fit)
@@ -93,7 +104,9 @@ b.mass<-1.833134
 Mi_hat<-(earwig_body_2$mean.mass)*(L0/earwig_body_2$mean.pronotum)^b.mass
 earwig_body_2$Mi_hat <- Mi_hat
 
-#### Egg size repeatability ####
+# =========================================
+# Egg size repeatability
+# =========================================
 
 # egg perimeter
 # make wide egg data long
@@ -129,74 +142,92 @@ earwig_egg_2 <- earwig_egg %>%
   mutate(mean.perim = mean(c_across(perim:perim3))) %>%
   ungroup()
 
-#### Final dataset ####
-
-total.data <- earwig_egg_2 %>%
-  inner_join(earwig_body_2, by="id") %>%
-  mutate(brood = recode(brood, "1" = "one", "2" = "two")) %>%
-  ungroup() %>%
-  dplyr::select(-c(num.x, num.y)) %>%
-  group_by(id,brood) %>%
-  mutate(num=n()) %>%
-  group_by(id, brood) %>%
-  summarise(egg.number=n(), mean.egg.size = mean(mean.perim),pc1=mean(pc1), mean.pro=mean(mean.pronotum), mean.massy=mean(mean.mass), mean.sci=mean(Mi_hat)) 
-
-df <- total.data %>%
-  group_by(id) %>%
-  mutate(
-    egg_number_between = mean(egg.number),
-    egg_number_within  = egg.number - egg_number_between
-  ) %>%
-  ungroup()
+# =========================================
+# Final dataset
+# =========================================
+# total.data <- earwig_egg_2 %>%
+#   inner_join(earwig_body_2, by="id") %>%
+#   mutate(brood = recode(brood, "1" = "one", "2" = "two")) %>%
+#   ungroup() %>%
+#   dplyr::select(-c(num.x, num.y)) %>%
+#   group_by(id,brood) %>%
+#   mutate(num=n()) %>%
+#   group_by(id, brood) %>%
+#   summarise(egg.number=n(), mean.egg.size = mean(mean.perim),pc1=mean(pc1), mean.pro=mean(mean.pronotum), mean.mass=mean(mean.mass), mean.sci=mean(Mi_hat)) 
+# 
+# df <- total.data %>%
+#   group_by(id) %>%
+#   mutate(
+#     egg_number_between = mean(egg.number),
+#     egg_number_within  = egg.number - egg_number_between
+#   ) %>%
+#   ungroup()
 
 # saveRDS(df, file = "data/processed/df.rds")
+df <- readRDS(file = "data/processed/df.rds")
 
-#### Effect of brood number ####
+# =========================================
+# Summary stats
+# =========================================
 
-priors_mv2 <- c(
-  
-  # Gaussian response: mean.egg.size
-  
-  # Intercept
-  prior(normal(0, 5), class = "Intercept", resp = "meaneggsize"),
-  
-  # Brood effect
-  prior(normal(0, 2), class = "b", resp = "meaneggsize"),
-  
-  # Random intercept SD
-  prior(student_t(3, 0, 2.5), class = "sd", resp = "meaneggsize"),
-  
-  # Residual SD
-  prior(student_t(3, 0, 2.5), class = "sigma", resp = "meaneggsize"),
-  
-  
-  # Poisson response: egg.number
-  # (log link scale!)
-  
-  # Intercept (on log scale)
-  prior(normal(2, 1.5), class = "Intercept", resp = "eggnumber"),
-  
-  # Brood effect (log scale)
-  prior(normal(0, 0.5), class = "b", resp = "eggnumber"),
-  
-  # Random intercept SD (log scale)
-  prior(student_t(3, 0, 1), class = "sd", resp = "eggnumber")
-)
+summary.stat <- df %>%
+  group_by(brood) %>%
+  summarize(mean.size = mean(mean.egg.size), 
+            sd.size   = sd(mean.egg.size, na.rm = TRUE),
+            n.size    = n(),
+            se.size   = sd.size / sqrt(n.size),
+            mean.num = mean(egg.number), 
+            sd.num   = sd(egg.number, na.rm = TRUE),
+            n.num    = n(),
+            se.num   = sd.num / sqrt(n.num),)
 
+# =========================================
+# Effect of brood number
+# =========================================
+# priors_mv2 <- c(
+#   
+#   # Gaussian response: mean.egg.size
+#   
+#   # Intercept
+#   prior(normal(0, 5), class = "Intercept", resp = "meaneggsize"),
+#   
+#   # Brood effect
+#   prior(normal(0, 2), class = "b", resp = "meaneggsize"),
+#   
+#   # Random intercept SD
+#   prior(student_t(3, 0, 2.5), class = "sd", resp = "meaneggsize"),
+#   
+#   # Residual SD
+#   prior(student_t(3, 0, 2.5), class = "sigma", resp = "meaneggsize"),
+#   
+#   
+#   # Poisson response: egg.number
+#   # (log link scale!)
+#   
+#   # Intercept (on log scale)
+#   prior(normal(2, 1.5), class = "Intercept", resp = "eggnumber"),
+#   
+#   # Brood effect (log scale)
+#   prior(normal(0, 0.5), class = "b", resp = "eggnumber"),
+#   
+#   # Random intercept SD (log scale)
+#   prior(student_t(3, 0, 1), class = "sd", resp = "eggnumber")
+# )
+# 
 # m_mv_2 <- brm(data = df,
 #               family = list(gaussian(), poisson()),
 #               bf(mvbind(mean.egg.size, egg.number) ~ 1 + brood + (1|id)) + set_rescor(FALSE),
-#               backend = "cmdstanr",
 #               prior = priors_mv2,
 #               file = "data/processed/m_mv_2",
+#               backend = "cmdstanr",
 #               chains=4,cores=4,iter = 4000, warmup= 1000)
 # print(summary(m_mv_2), digits = 4)
 m_mv_2 <- readRDS(file = "data/processed/m_mv_2.rds")
 m_mv_2.sum = tidy(m_mv_2, effects = "fixed")
 
-m_mv_2.sum
-#### Multivariate model for residual correlation ####
-
+# =========================================
+# Multivariate model for residual correlation
+# =========================================
 priors_mv <- c(
   
   # FIXED EFFECTS (egg size)
@@ -230,9 +261,9 @@ m_mv <- brm(
   f_size + f_number + set_rescor(TRUE),
   data = df,
   family = gaussian(),
-  backend = "cmdstanr",
   prior = priors_mv,
   file = "data/processed/m_mv",
+  backend = "cmdstanr",
   control = list(adapt_delta = 0.999),
   chains = 4,
   cores = 4,
@@ -242,7 +273,21 @@ m_mv <- brm(
 print(summary(m_mv), digits = 4)
 m_mv <- readRDS(file = "data/processed/m_mv.rds")
 
-#### Raw phenotypic correlation (all broods) ####
+# Extract posterior draws
+post <- as_draws_df(m_mv)
+
+# Extract residual correlation draws
+rescor_draws <- post$rescor__meaneggsize__eggnumber
+
+# Posterior mean
+r_within <- mean(rescor_draws)
+
+# 95% credible interval
+within_credible <- quantile(rescor_draws, probs = c(0.025, 0.975))
+
+# =========================================
+# Raw phenotypic correlation (all broods)
+# =========================================
 
 # Function to compute Pearson correlation
 corr_raw_fun <- function(data, indices) {
@@ -264,8 +309,9 @@ r_raw_ci95 <- ci_raw_95$percent[4:5]  # 2.5% and 97.5% percentiles
 cat("Raw phenotypic correlation (r_raw) = ", round(r_raw, 3),
     ", 95% CI = [", round(r_raw_ci95[1], 3), ", ", round(r_raw_ci95[2], 3), "]\n")
 
-#### Between-female correlation ####
-
+# =========================================
+# Between-female correlation
+# =========================================
 # Compute female means
 df_female <- df %>%
   group_by(id) %>%
@@ -328,7 +374,9 @@ cat("Between-female correlation (r_between) = ", round(r_between, 3),
 # mean(r_between_total)
 # quantile(r_between_total, c(.025, .975))
 
-#### Fixed-effects model ####
+# =========================================
+# Fixed-effects model
+# =========================================
 
 # priors <- c(
 #   prior(normal(0, 0.01), class = "b", coef = "egg_number_within"),
@@ -339,93 +387,99 @@ cat("Between-female correlation (r_between) = ", round(r_between, 3),
 # )
 # 
 # fit.null <- brm(
-#   mean.egg.size ~ 
-#     egg_number_within + 
-#     egg_number_between + 
+#   mean.egg.size ~
+#     egg_number_within +
+#     egg_number_between +
 #     (1 | id),
 #   data = df,
 #   family = gaussian(),
 #   prior = priors,
-#   backend = "cmdstanr",
 #   control = list(adapt_delta = 0.999),
 #   save_pars = save_pars(all = TRUE),
 #   file = "data/processed/fit.null",
+#   backend = "cmdstanr",
 #   chains = 4, cores = 4, iter = 4000
 # )
 # print(summary(fit.null), digits = 4)
 # fit.null <- add_criterion(fit.null, "loo", moment_match=TRUE) # best model
-# 
+fit.null <- readRDS(file = "data/processed/fit.null.rds")
+
 # fit.pro <- brm(
-#   mean.egg.size ~ 
-#     egg_number_within + 
+#   mean.egg.size ~
+#     egg_number_within +
 #     egg_number_between + scale(mean.pro) +
 #     (1 | id),
 #   data = df,
-#   backend = "cmdstanr",
 #   control = list(adapt_delta = 0.999),
 #   save_pars = save_pars(all = TRUE),
 #   family = gaussian(),
 #   file = "data/processed/fit.pro",
+#   backend = "cmdstanr",
 #   chains = 4, cores = 4,iter = 4000
 # )
 # print(summary(fit.pro), digits = 4)
 # fit.pro <- add_criterion(fit.pro, "loo", moment_match=TRUE)
 # 
 # fit.mass <- brm(
-#   mean.egg.size ~ 
-#     egg_number_within + 
-#     egg_number_between + scale(mean.massy) +
+#   mean.egg.size ~
+#     egg_number_within +
+#     egg_number_between + scale(mean.mass) +
 #     (1 | id),
 #   data = df,
-#   backend = "cmdstanr",
 #   control = list(adapt_delta = 0.999),
 #   save_pars = save_pars(all = TRUE),
 #   family = gaussian(),
 #   file = "data/processed/fit.mass",
+#   backend = "cmdstanr",
 #   chains = 4, cores = 4,iter = 4000
 # )
 # print(summary(fit.mass), digits = 4)
 # fit.mass <- add_criterion(fit.mass, "loo", moment_match=TRUE)
 # 
 # fit.pc1 <- brm(
-#   mean.egg.size ~ 
-#     egg_number_within + 
+#   mean.egg.size ~
+#     egg_number_within +
 #     egg_number_between + scale(pc1) +
 #     (1 | id),
 #   data = df,
-#   backend = "cmdstanr",
 #   control = list(adapt_delta = 0.999),
 #   save_pars = save_pars(all = TRUE),
 #   family = gaussian(),
 #   file = "data/processed/fit.pc1",
+#   backend = "cmdstanr",
 #   chains = 4, cores = 4,iter = 4000
 # )
 # print(summary(fit.pc1), digits = 4)
 # fit.pc1 <- add_criterion(fit.pc1, "loo", moment_match=TRUE)
 # 
-# fit.sci <- brm(
-#   mean.egg.size ~ 
-#     egg_number_within + 
+# fit.smi <- brm(
+#   mean.egg.size ~
+#     egg_number_within +
 #     egg_number_between + scale(mean.sci) +
 #     (1 | id),
 #   data = df,
-#   backend = "cmdstanr",
 #   control = list(adapt_delta = 0.999),
 #   save_pars = save_pars(all = TRUE),
 #   family = gaussian(),
-#   file = "data/processed/fit.sci",
+#   file = "data/processed/fit.smi",
+#   backend = "cmdstanr",
 #   chains = 4, cores = 4,iter = 4000
 # )
-# print(summary(fit.sci), digits = 4)
-# fit.sci <- add_criterion(fit.sci, "loo", moment_match=TRUE)
+# print(summary(fit.smi), digits = 4)
+# fit.smi <- add_criterion(fit.smi, "loo", moment_match=TRUE)
 # 
-# model.comparison <- loo_compare(fit.null, fit.pro, fit.mass, fit.pc1,fit.sci, criterion = "loo")
+# model.comparison <- loo_compare(fit.null, fit.pro, fit.mass, fit.pc1, fit.smi, criterion = "loo")
+# saveRDS(model.comparison, file = "data/processed/model.comparison.rds")
+model.comparison <- readRDS(file = "data/processed/model.comparison.rds")
+
 # model.comparison.report <-report(model.comparison) # fit.null is best model
 # saveRDS(model.comparison.report, file = "data/processed/model.comparison.report.rds")
 fit.null <- readRDS(file = "data/processed/fit.null.rds")
 model.comparison.report <- readRDS(file = "data/processed/model.comparison.report.rds")
 
-#### Standardized effects ####
+# =========================================
+# Standardized effects
+# =========================================
 
 post <- as_draws_df(fit.null)
 sd_y <- sd(df$mean.egg.size)
@@ -448,7 +502,9 @@ std_effects_table <- data.frame(
 )
 # saveRDS(std_effects_table, file = "data/processed/std_effects_table.rds")
 
-#### Brood adjusted slope ####
+# =========================================
+# Brood adjusted slope
+# =========================================
 
 m_wb_brood <- brm(
   mean.egg.size ~ 
@@ -457,9 +513,10 @@ m_wb_brood <- brm(
     brood +
     (1 | id),
   data = df,
-  backend = "cmdstanr",
   family = gaussian(),
+  backend = "cmdstanr",
   chains = 4, cores = 4,
+  file = "data/processed/m_wb_brood",
   iter = 4000, warmup = 1000
 )
 fixef(m_wb_brood)
@@ -473,7 +530,6 @@ m_rs <- brm(
     egg_number_between +
     (1 + egg_number_within | id),
   data = df,
-  backend = "cmdstanr",
   family = gaussian(),
   prior = c(
     prior(normal(0, 0.5), class = "b"),
@@ -545,4 +601,138 @@ print(summary(m_rs), digits = 4)
 #   theme_classic()
 
 ## ---- end
+
+# =========================================
+# 3-panel plot
+# =========================================
+# between-female correlation
+
+# df_between <- df %>%
+#   group_by(id) %>%
+#   summarise(
+#     mean_egg_number = mean(egg.number, na.rm = TRUE),
+#     mean_egg_size   = mean(mean.egg.size, na.rm = TRUE),
+#     .groups = "drop"
+#   )
+# 
+# p_between <- ggplot(df_between, aes(x = mean_egg_number, y = mean_egg_size)) +
+#   geom_point(size = 2) +
+#   geom_smooth(method = "lm", se = TRUE) +
+#   labs(
+#     x = "Mean egg number per female",
+#     y = "Mean egg size per female",
+#     title = "Between-female correlation"
+#   ) +
+#   theme_classic()
+# ggsave(p_between, filename="p_between.jpg", width=10.83, height=10.83, dpi=300,antialias="default")    
+# 
+# # within-female correlation with reaction norms
+# 
+# newdata_within <- tibble(
+#   egg_number_within  = seq(
+#     min(df$egg_number_within, na.rm = TRUE),
+#     max(df$egg_number_within, na.rm = TRUE),
+#     length.out = 100
+#   ),
+#   egg_number_between = 0
+# )
+# 
+# pred_within <- m_rs %>%
+#   add_epred_draws(
+#     newdata = newdata_within,
+#     re_formula = NA   # population-level only
+#   ) %>%
+#   group_by(egg_number_within) %>%
+#   summarise(
+#     mean = mean(.epred),
+#     lwr  = quantile(.epred, 0.025),
+#     upr  = quantile(.epred, 0.975),
+#     .groups = "drop"
+#   ) %>%
+#   rename(egg_n_within = egg_number_within)
+# 
+# p_within <- ggplot(df_within,
+#                    aes(x = egg_n_within,
+#                        y = mean.egg.size,
+#                        group = id)) +
+#   geom_line(alpha = 0.3) +
+#   geom_point(alpha = 0.4, size = 1) +
+#   
+#   # Posterior ribbon (population-level)
+#   geom_ribbon(
+#     data = pred_within,
+#     aes(x = egg_n_within, ymin = lwr, ymax = upr),
+#     inherit.aes = FALSE,
+#     alpha = 0.25
+#   ) +
+#   
+#   # Posterior mean line (population-level)
+#   geom_line(
+#     data = pred_within,
+#     aes(x = egg_n_within, y = mean),
+#     inherit.aes = FALSE,
+#     linewidth = 1
+#   ) +
+#   
+#   labs(
+#     x = "Within-female deviation in egg number",
+#     y = "Mean egg size",
+#     title = "B) Within-female plasticity (posterior mean)"
+#   ) +
+#   theme_classic(base_size = 12)
+# ggsave(p_within, filename="p_within.jpg", width=10.83, height=10.83, dpi=300,antialias="default")    
+# 
+# # residual correlation plot
+# 
+# # m_mv = your multivariate brms model with responses: mean.egg.size and egg.number
+# 
+# # Get posterior residuals (array: draws x observations x response)
+# resid_array <- residuals(m_mv, summary = FALSE)  # do NOT summarize yet
+# 
+# # Compute posterior mean residual for each observation
+# resid_mean <- apply(resid_array, c(2, 3), mean)  # dims: obs x response
+# 
+# # Convert to data frame, combine with original df
+# df_resid <- df %>%
+#   mutate(
+#     resid_egg_size   = as.numeric(resid_mean[, "meaneggsize"]),
+#     resid_egg_number = as.numeric(resid_mean[, "eggnumber"])
+#   )
+# 
+# # Posterior draws of the residual correlation
+# rescor_draws <- posterior_samples(m_mv, pars = "rescor__meaneggsize__eggnumber")
+# 
+# # Extract numeric vector
+# rescor_vec <- rescor_draws[[1]]
+# 
+# # Summarize posterior: mean + 95% credible interval
+# rescor_summary <- c(
+#   mean = mean(rescor_vec),
+#   lwr  = quantile(rescor_vec, 0.025),
+#   upr  = quantile(rescor_vec, 0.975)
+# )
+# 
+# rescor_summary
+# 
+# p_resid <- ggplot(df_resid, aes(x = resid_egg_number, y = resid_egg_size)) +
+#   geom_point(alpha = 0.5, size = 2) +  # residual points
+#   geom_smooth(method = "lm", se = FALSE, color = "black", linewidth = 1) +  # trend line
+#   labs(
+#     x = "Residual egg number",
+#     y = "Residual egg size",
+#     title = paste0(
+#       "Residual correlation (r = ",
+#       round(rescor_summary["mean"], 2),
+#       ", 95% CrI [",
+#       round(rescor_summary["lwr.2.5%"], 2), ", ",
+#       round(rescor_summary["upr.97.5%"], 2), "])"
+#     )
+#   ) +
+#   theme_classic(base_size = 12)
+# ggsave(p_resid, filename="p_resid.jpg", width=10.83, height=10.83, dpi=300,antialias="default")    
+# 
+
+
+
+
 
